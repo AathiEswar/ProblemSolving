c75cd2914f8c33f5ab26e9124cf0e16961f38a9e
AathiEswar <aathi28082003@gmail.com>
2024-07-06 20:59:33 +0530

    6-7-2024

--- Graphs/FindCenterGreedy.java
+++ Graphs/FindCenterGreedy.java
@@ -4,7 +4,6 @@ public class FindCenterGreedy {
     public int findCenter(int[][] edges) {
 
 
-        return edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1] ?edges[0][0] :edges[0][1];
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
 
-    }
-}
+    }}
