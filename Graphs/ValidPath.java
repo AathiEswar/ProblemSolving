929d918a20f60fedd64560a579f3077fe5a40bad
AathiEswar <aathi28082003@gmail.com>
2024-07-19 16:34:03 +0530

    19-7-2024

---
+++ Graphs/ValidPath.java
@@ -0,0 +1,40 @@
+package ProblemSolving.Graphs;
+import java.util.*;
+public class ValidPath {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        // create a storing list and populate it
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        for(int i = 0 ; i<n;i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int i = 0;i<edges.length;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adjList.get(v).add(u);
+            adjList.get(u).add(v);
+        }
+
+        // use bfs to find the visited array
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(source);
+        visited[source] = true;
+
+        while(!queue.isEmpty()){
+            Integer node = queue.poll();
+
+            for(Integer adjNode : adjList.get(node)){
+                if(!visited[adjNode]){
+                    queue.offer(adjNode);
+                    visited[adjNode] = true;
+                }
+            }
+        }
+
+        return visited[destination];
+    }
+}
